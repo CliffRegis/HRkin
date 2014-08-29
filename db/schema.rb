808753c27b2fb90774373aab187f81b6ba58b6c9
Charlie Gaines <beaugaines@gmail.com>
2014-08-29 12:40:22 -0400

    fix faulty migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140829125848) do
+ActiveRecord::Schema.define(version: 20140829163853) do
 
   create_table "comments", force: true do |t|
     t.text     "content"
@@ -19,13 +19,24 @@ ActiveRecord::Schema.define(version: 20140829125848) do
     t.datetime "updated_at"
   end
 
+  create_table "friendships", force: true do |t|
+    t.integer  "popular_model_id"
+    t.string   "popular_model_type"
+    t.integer  "friend_id"
+    t.string   "friend_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "friendships", ["friend_id", "friend_type"], name: "index_friendships_on_friend_id_and_friend_type"
+  add_index "friendships", ["popular_model_id", "popular_model_type"], name: "index_friendships_on_popular_model_id_and_popular_model_type"
+
   create_table "posts", force: true do |t|
     t.string   "title"
     t.text     "content"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
-    t.integer  "comment_id"
   end
 
   add_index "posts", ["user_id"], name: "index_posts_on_user_id"
