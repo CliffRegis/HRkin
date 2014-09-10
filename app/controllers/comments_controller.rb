543ef0015d6d6fd92171c3e8e00174b20d707069
Charlie Gaines <beaugaines@gmail.com>
2014-09-10 17:06:05 -0400

    user id on comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -25,17 +25,14 @@ class CommentsController < ApplicationController
   # POST /comments.json
   def create
     @post = Post.find(params[:post_id])
-    @comment = Comment.new(comment_params)
-    @comments = @post.comments
-    
-    respond_to do |format|
-      if @comment.save
-        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
-        format.json { render :show, status: :created, location: @comment }
-      else
-        format.html { render :new }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
-      end
+    @topic = @post.topic
+    @comment = current_user.comments.build(comment_params)
+    @comment.post_id = @post.id
+    if @comment.save
+      redirect_to [@topic, @post], notice: 'Comment was successfully created.'
+    else
+      flash[:alert] = 'Comment creation failed'
+      redirect to [@topic, @post]
     end
   end
 
@@ -71,6 +68,6 @@ class CommentsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def comment_params
-      params.require(:comment).permit(:content)
+      params.require(:comment).permit(:content, :post_id, :user_id)
     end
 end
