2d3e841f223bcd0cd7a5269880183682501a48c7
Clifton Regis <cregisexcel@yahoo.com>
2014-12-03 14:05:43 -0500

    removed votes from Topic model

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,14 +1,12 @@
 class Topic < ActiveRecord::Base
   has_many :posts, dependent: :destroy
-  has_many :votes , dependent: :destroy
+  
 
   # searchable do
   #   text :title, :boost => 5
   #   text :content
   # end
 
-  def points
-    votes.sum(:value).to_i
-  end
+  
 
 end
