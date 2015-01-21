f59cf0ce206bee237f634bb10ebd5991e596dfc7
Clifton Regis <cregisexcel@yahoo.com>
2015-01-21 16:19:13 +0430

    change to dt in app.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -28,7 +28,7 @@ $ (document).ready(function() {
   });
 
 $ (document).ready(function() {
-  $('#postindex').DataTable();
-  });
+  $('#postindex').DataTable( {
+  "searching": true
+   } );
   
-});
