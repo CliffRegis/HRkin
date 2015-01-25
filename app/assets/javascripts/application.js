be73930641645e38adb18608c9367174cd5b0e79
Clifton Regis <cregisexcel@yahoo.com>
2015-01-25 22:54:19 +0430

    added line 15 to app.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap
 //= require jquery.turbolinks
 //= require dataTables/bootstrap/3/jquery.dataTables.bootstrap
 //= require turbolinks
