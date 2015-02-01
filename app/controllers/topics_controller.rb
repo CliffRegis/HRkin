27495acfc678d0cca1229ee923584772c31d47a1
Clifton Regis <cregisexcel@yahoo.com>
2015-02-01 12:35:41 +0430

    added post_id to topic show

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -13,7 +13,7 @@ helper_method :sort_column, :sort_direction
   def show
     @topics = Topic.paginate(page: params[:page], per_page: 10)
     @topic = Topic.find(params[:id])
-    @post = Post.find(params[:id])
+    @post = Post.find(params[:post_id])
     @posts = @topic.posts
     @comments = @post.comments
   end
