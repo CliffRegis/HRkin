c757d6bc01ad6e3f0ae626346edde0497391d05b
Clifton Regis <cregisexcel@yahoo.com>
2015-02-04 12:50:20 +0430

    various changes

--- app/controllers/walls_controller.rb
+++ app/controllers/walls_controller.rb
@@ -3,7 +3,6 @@ class WallsController < ApplicationController
   def show
     @friends = current_user.followings
     @posts = Post.where(user_id: current_user.friends.pluck(:id))
-    
   end
 end
   
